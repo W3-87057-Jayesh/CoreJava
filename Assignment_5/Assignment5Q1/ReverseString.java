3f8a7a4320d137807508e30df69b31237b9161ac
Jayesh_Inamdar <jayeshinamdar316@gmail.com>
2024-09-10 17:44:29 +0530

    Assignment 5 question 1 Submitted

---
+++ Assignment_5/Assignment5Q1/ReverseString.java
@@ -0,0 +1,11 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String str = "JAyesh";
+        StringBuffer sbr = new StringBuffer(str);
+        sbr.reverse();
+        System.out.println(sbr);
+	}
+
+}
