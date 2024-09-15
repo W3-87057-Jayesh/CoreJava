ec5d1a76dae9d125cb1824eb5044035284cfcfeb
Jayesh_Inamdar <jayeshinamdar316@gmail.com>
2024-09-16 01:26:39 +0530

    Assignment 9 Q5 Submitted

---
+++ Assignment_9/AssignmentQ5/TreeColour.java
@@ -0,0 +1,18 @@
+import java.util.TreeSet;
+
+public class TreeColour {
+
+	public static void main(String[] args) {
+		 TreeSet<String> clr = new TreeSet<>();
+
+	        clr.add("Red");
+	        clr.add("Green");
+	        clr.add("Blue");
+	        clr.add("Yellow");
+	        clr.add("Purple");
+	        clr.add("Orange");
+
+	        System.out.println("TreeSet of clr: " + clr);
+	}
+
+}
